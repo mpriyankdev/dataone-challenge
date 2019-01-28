66d5bf606761f7cd058be80150a68d4d1c5f0d34
Priyank Maheshwari <mpriyank.dev@gmail.com>
2019-01-28 21:38:43 +0530

    Initial Project Skeleton Commit

---
+++ json-transformer/src/main/java/com/dataone/jsontransformer/JsonTransformerApplication.java
@@ -0,0 +1,14 @@
+package com.dataone.jsontransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonTransformerApplication.class, args);
+	}
+
+}
+
